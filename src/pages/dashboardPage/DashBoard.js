ce7be79da1d88d10a317624f89b6c669146abefb
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2022-03-28 07:26:04 +0600

    dashboard page responsive done

--- src/pages/dashboardPage/DashBoard.js
+++ src/pages/dashboardPage/DashBoard.js
@@ -2,6 +2,7 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { Dropdown } from "bootstrap";
 import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import Footer from "../../components/footer/Footer";
 import NavBar from "../../components/navBar/NavBar";
 import Pagination from "../../components/pagination/Pagination";
@@ -34,25 +35,30 @@ const DashBoard = () => {
   return (
     <div>
       {/* the nav section */}
-      <section>
-        <NavBar />
-        <div className="d-flex align-items-center justify-content-between mt-4">
-          <h2 className="dashboard-heading ms-5 ps-5">Dashboards</h2>
-          <div className="search-button-container me-5 pe-5 d-flex align-items-center">
+
+      <NavBar />
+      <Container fluid>
+        <Row className="mt-2 flex-wrap border border-danger">
+          <Col className="mt-2 order-1" lg={5} md={5} sm={12} xs={12}>
+            <h2 className="dashboard-heading ms-5">Dashboards</h2>
+          </Col>
+          <Col className="mt-2 order-3" lg={3} md={3} sm={12} xs={12}>
             <input
               placeholder="&#xF002;            Search Dashboard"
               className="me-5 searchInput form-control"
               type="text"
             />
+          </Col>
+          <Col className="mt-2 order-2" lg={3} md={3} sm={12} xs={12}>
             <button
               onClick={handleNewDashboard}
               className="custom-btn  btn ms-5"
             >
               <FontAwesomeIcon icon={faPlus} /> New Dashboard
             </button>
-          </div>
-        </div>
-      </section>
+          </Col>
+        </Row>
+      </Container>
 
       {/* pagination sorby and view section  */}
       <section className="d-flex align-items-center justify-content-between mt-5 ">

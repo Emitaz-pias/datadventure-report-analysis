b659166ea2b63d47f7ebb4021179f8a377d8cf53
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2022-03-28 23:45:48 +0600

    unsued code removed for netlify deploy

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 import logo from "../../images/logo.png";

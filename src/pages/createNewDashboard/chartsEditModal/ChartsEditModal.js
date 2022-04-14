40c1d96dc9bd1ef194e31820660b0e0384368fc3
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2022-04-14 22:30:14 +0600

    first row of quesiton select and current chart done

--- src/pages/createNewDashboard/chartsEditModal/ChartsEditModal.js
+++ src/pages/createNewDashboard/chartsEditModal/ChartsEditModal.js
@@ -1,5 +1,9 @@
+import { faCross, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Col, Dropdown, Row } from "react-bootstrap";
 import Modal from "react-modal";
+import "./ChartsEditModal.css";
 
 const ChartsEditModal = ({ modalIsOpen, closeMdal }) => {
   const customStyles = {
@@ -14,17 +18,69 @@ const ChartsEditModal = ({ modalIsOpen, closeMdal }) => {
   };
   Modal.setAppElement("#root");
   return (
-    <div>
-      {" "}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeMdal}
-        style={customStyles}
-        contentLabel=" charts Modal"
-      >
-        <h1>some in the modal</h1>
-      </Modal>
-    </div>
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeMdal}
+      style={customStyles}
+      contentLabel=" charts Modal"
+    >
+      <main className="fullModal">
+        {/* // close switch and tile */}
+        <div className="d-flex justify-content-between">
+          <h3>Edit Widget</h3>
+          <FontAwesomeIcon
+            icon={faTimes}
+            size="2x"
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+        <Row className="mt-3">
+          <Col md={2}>
+            <h5 className="mb-3">Enter Graph Title</h5>
+            <input
+              className="ms-1 form-control"
+              style={{
+                height: "2.7em",
+              }}
+              type="text"
+            ></input>
+          </Col>
+          <Col className="ms-4" md={4}>
+            <h5 className="mb-3">Select Specific Question</h5>
+            <Dropdown>
+              <Dropdown.Toggle className="custom-dropdown" id="dropdown-basic">
+                Question
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="custom-dropdown-menu">
+                <Dropdown.Item href="#/action-1">English</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Bangla</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Arabic</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+
+          <Col className="ms-5 ps-5 mt-3" md={4}>
+            <div className="ms-5 ps-5 mt-4">
+              <Dropdown>
+                <Dropdown.Toggle
+                  className="custom-dropdown"
+                  id="dropdown-basic"
+                >
+                  Timeview Selection
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu className="custom-dropdown-menu">
+                  <Dropdown.Item href="#/action-1">English</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Bangla</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Arabic</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </Col>
+        </Row>
+      </main>
+    </Modal>
   );
 };
 
